b3ff5fae447adc3f0fa02b31cb52f5ef50b6bef7
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2023-12-14 11:02:18 +0530

    Initial version

---
+++ src/test/java/com/octobit8/microserviceapp/MicroserviceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octobit8.microserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
